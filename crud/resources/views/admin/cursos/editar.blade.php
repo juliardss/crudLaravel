94e7c1e89d0de00fe1e69b309df9c83eb6bfbacd
Ana Carolina <ana.torejani@unesp.br>
2026-05-09 23:01:46 -0300

    segundo commit

---
+++ crud/resources/views/admin/cursos/editar.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.site')
+
+@section('titulo', 'Cursos')
+
+@section('conteudo')
+
+<div class="container">
+    <h3 class="center">Editando Curso</h3>
+
+    <div class="row">
+        <form action="{{ route('admin.cursos.atualizar', $linha->id) }}"
+              method="post"
+              enctype="multipart/form-data">
+
+            {{ csrf_field() }}
+
+            <input type="hidden" name="_method" value="put">
+
+            @include('admin.cursos._form')
+
+            <button class="btn pink lighten-3">
+                Atualizar
+            </button>
+
+        </form>
+    </div>
+</div>
+
+@endsection
